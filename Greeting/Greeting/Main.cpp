31d1ce6192cdd73e5e0add727e0f198b0669a943
tib66 <t.zaragoci@mailo.fr>
2026-06-27 14:58:17 +0200

    Update Main.cpp

--- Greeting/Greeting/Main.cpp
+++ Greeting/Greeting/Main.cpp
@@ -4,6 +4,7 @@
 int main() {
 	std::cout << "Please enter your name" << std::endl;
 	std::string name;
+	std::string answer_by_age;
 	std::cin >> name;
 	std::cout << "Enter your age ";
 	int age;
@@ -14,9 +15,20 @@ int main() {
 		std::cout << "Enter	a valid age (a positive or null number) " << std::endl;
 		std::cin >> age;
 
-
+	}
+	if (age < 25){
+		answer_by_age = ", you are only " + std::to_string(age) + " years old.";
+	}
+	else if (age < 50) {
+		answer_by_age = ", you are already " + std::to_string(age) + " years old.";
+	}
+	else {
+		answer_by_age = ",  you are very wise for your age.";
 	}
 
 
-	std::cout << "Your name is " << name << " and you are "<<age<<"." << std::endl;
+
+	std::cout << "Hello " << name << answer_by_age<< std::endl;
+	
+
 }
